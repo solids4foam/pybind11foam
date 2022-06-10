301bc1370a2a5b199c1de06c9f88e3d62dddf4e5
Simon Rodriguez <simon.rodriguezluzardo@ucdconnect.ie>
2022-06-10 20:20:44 +0100

    Added setInletVelocity.py

--- tutorials/pythonCavity/withPython/fineMesh/setInletVelocity.py
+++ tutorials/pythonCavity/withPython/fineMesh/setInletVelocity.py
@@ -1,10 +1,14 @@
 import numpy as np
 
 def calculate(face_centres, time):
-    # Initialise result
+    # Return if the array size in zero                                                                                            
+    if face_centres.shape[0] == 0:
+        return face_centres
+
+    # Initialise result                                                                                                           
     result = np.zeros(shape = face_centres.shape)
 
-    # Calculate values using the x coordinates and time
+    # Calculate values using the x coordinates and time                                                                           
     x = face_centres[:, 0]
     result[:,0] = np.sin(time * np.pi) * np.sin(x * 40 * np.pi)
 
