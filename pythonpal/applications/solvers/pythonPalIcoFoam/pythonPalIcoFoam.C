d96f47d6615f53afe6eca7ff6068eba07195c78e
Simon Rodriguez <simon.rodriguezluzardo@ucdconnect.ie>
2022-11-15 16:17:14 +0000

    removed pythonpal folder, added pythonpal submodule

--- pythonpal/applications/solvers/pythonPalIcoFoam/pythonPalIcoFoam.C
+++
@@ -1,7 +0,0 @@
-#ifdef OPENFOAMFOUNDATION
-    #include "pythonPalIcoFoam.foundation.C"
-#elif OPENFOAMESI
-    #include "pythonPalIcoFoam.esi.C"
-#else
-    #include "pythonPalIcoFoam.foamextend.C"
-#endif
