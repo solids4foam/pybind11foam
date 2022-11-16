4fe93035200962d0adecce2c5988b25322593a64
Simon Rodriguez <simon.rodriguezluzardo@ucdconnect.ie>
2022-11-16 15:17:20 +0000

    Minor fix, pythonVelocity header

--- src/pythonVelocity/pythonVelocity.C
+++ src/pythonVelocity/pythonVelocity.C
@@ -1,18 +1,42 @@
 /* License
-    This file is part of OpenFOAM.
+    This program is part of pythonPal4Foam.
 
-    OpenFOAM is free software: you can redistribute it and/or modify it
-    under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+    This program is free software: you can redistribute it and/or modify 
+    it under the terms of the GNU General Public License as published 
+    by the Free Software Foundation, either version 3 of the License, 
+    or (at your option) any later version.
 
-    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
-    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-    for more details.
+    This program is distributed in the hope that it will be useful, 
+    but WITHOUT ANY WARRANTY; without even the implied warranty of 
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
 
-    You should have received a copy of the GNU General Public License
-    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+    See the GNU General Public License for more details. You should have 
+    received a copy of the GNU General Public License along with this 
+    program. If not, see <https://www.gnu.org/licenses/>. 
+
+Class
+    Foam::pythonVelocity
+
+Description
+    This velocity inlet boundary condition is calls a python script to set
+    the velocity profile via python using pybind11.
+
+    The address of the python script is passed as an argument.
+
+Usage
+    Example of the boundary condition specification:
+    \verbatim
+    <patchName>
+    {
+        type            pythonVelocity;
+        pythonScript    "$FOAM_CASE/myPythonScript.py";
+        value           uniform 0;
+    }
+    \endverbatim
+
+Author
+    Philip Cardiff, UCD.
+    Simón A. Rodríguez L., UCD.
 
 \*---------------------------------------------------------------------------*/
 
