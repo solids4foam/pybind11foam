13d2093f4105618c6b80b9db95081c49d24e0706
Simon Rodriguez <simon.rodriguezluzardo@ucdconnect.ie>
2022-11-16 15:14:37 +0000

    Minor fix pythonLinearElastic's header

--- src/pythonLinearElastic/pythonLinearElastic.C
+++ src/pythonLinearElastic/pythonLinearElastic.C
@@ -1,18 +1,32 @@
 /* License
-    This file is part of foam-extend.
+    This program is part of pythonPal4Foam.
 
-    foam-extend is free software: you can redistribute it and/or modify it
-    under the terms of the GNU General Public License as published by the
-    Free Software Foundation, either version 3 of the License, or (at your
-    option) any later version.
+    This program is free software: you can redistribute it and/or modify 
+    it under the terms of the GNU General Public License as published 
+    by the Free Software Foundation, either version 3 of the License, 
+    or (at your option) any later version.
 
-    foam-extend is distributed in the hope that it will be useful, but
-    WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    General Public License for more details.
+    This program is distributed in the hope that it will be useful, 
+    but WITHOUT ANY WARRANTY; without even the implied warranty of 
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
 
-    You should have received a copy of the GNU General Public License
-    along with foam-extend.  If not, see <http://www.gnu.org/licenses/>.
+    See the GNU General Public License for more details. You should have 
+    received a copy of the GNU General Public License along with this 
+    program. If not, see <https://www.gnu.org/licenses/>. 
+
+Class
+    pythonLinearElastic
+
+Description
+    Mechanical law that uses pybind11 to call a trained Keras (TensorFlow)
+    neural network mechanical law.
+
+SourceFiles
+    pythonLinearElastic.C
+
+Author
+    Simon A. Rodriguez, UCD. All rights reserved
+    Philip Cardiff, UCD. All rights reserved
 
 \*---------------------------------------------------------------------------*/
 
